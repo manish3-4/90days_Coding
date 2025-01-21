2ed410dd835d4c77dace8fbf98f716915b7ad4c6
manish3-4 <preetisikka553@gmail.com>
2025-01-22 02:07:42 +0530

    day6

---
+++ day6/Hi.java
@@ -0,0 +1,8 @@
+package day6;
+import day6.Hello;
+public class Hi {
+    public static void main(String[] args) {
+        Hello h = new Hello();
+        h.message();
+    }
+}
--- day6/hello.java
+++ day6/hello.java
@@ -1,4 +1,6 @@
-package day
-public class hello{
-
+package day6;
+public class Hello {
+ public void message(){
+    System.out.println("hello manish ji");
+ }
 }
