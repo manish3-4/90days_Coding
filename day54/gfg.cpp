775848c09d05bb9ea878315111370ac3a712f67c
manish3-4 <preetisikka553@gmail.com>
2025-03-09 23:54:48 +0530

    day54

---
+++ day54/gfg.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GfG
+{
+public
+    static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0)
+        {
+            String s = sc.next();
+
+            Solution obj = new Solution();
+
+            System.out.println(obj.countPS(s));
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+class Solution
+{
+public
+    int countPS(String s)
+    {
+        int n = s.length();
+        int count = 0;
+
+        // Expand Around Center to count palindromes
+        for (int i = 0; i < n; i++)
+        {
+            // Count even length palindromes
+            count += expandAroundCenter(s, i, i + 1);
+            // Count odd length palindromes (excluding single-character substrings)
+            count += expandAroundCenter(s, i, i);
+        }
+        return count;
+    }
+
+private
+    int expandAroundCenter(String s, int left, int right)
+    {
+        int count = 0;
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right))
+        {
+            if (right - left + 1 >= 2)
+            { // Consider only length >= 2
+                count++;
+            }
+            left--;
+            right++;
+        }
+        return count;
+    }
+}
